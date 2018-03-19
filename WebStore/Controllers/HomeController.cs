710c1f8f057ae62c4eb62cd2ab49cc01d462608c
smolenz <33202698+smolenz@users.noreply.github.com>
2018-03-19 20:46:25 +0300

    HomeWork_1

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+{
+        new EmployeeView
+        {
+        Id = 1,
+        FirstName = "Иван",
+        SurName = "Иванов",
+        Patronymic = "Иванович",
+        Age = 22,
+        City= "Москва",
+        Hobby="Аквариумистика"
+        },
+        new EmployeeView
+        {
+        Id = 2,
+        FirstName = "Владислав",
+        SurName = "Петров",
+        Patronymic = "Иванович",
+        Age = 35,
+        City= "Иваново",
+        Hobby="Моделизм"
+        }
+        };
+
+        public IActionResult Index()
+        {
+            return View(_employees);
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(_employees.Where(x=>x.Id==id).FirstOrDefault());
+        }
+    }
+}
---
+++ WebStore/Models/EmployeeView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    public class EmployeeView
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Имя:")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Фамилия:")]
+        public string SurName { get; set; }
+
+        [Display(Name = "Отчество:")]
+        public string Patronymic { get; set; }
+
+        [Display(Name = "Возраст:")]
+        public int Age { get; set; }
+
+        [Display(Name = "Город:")]
+        public string City { get; set; }
+
+        [Display(Name = "Хобби:")]
+        public string Hobby { get; set; }
+    }
+}
